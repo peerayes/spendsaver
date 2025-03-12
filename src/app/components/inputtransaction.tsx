880425dc85d0f0457ec82cf3f7570e31310d18f5
Peerayes <peerayes@gmail.com>
2025-03-12 22:42:20 +0700

    Fix radio button issue on mobile devices

--- src/app/components/inputtransaction.tsx
+++ src/app/components/inputtransaction.tsx
@@ -41,7 +41,6 @@ const InputTranSactions: React.FC<InputTransactionsProps> = ({
   const [calculation, setCalculation] = useState("");
 
   useEffect(() => {
-    setSelectedOption("income");
     if (text && calculation) {
       setIsSaving(false);
     } else {
@@ -191,25 +190,7 @@ const InputTranSactions: React.FC<InputTransactionsProps> = ({
                     <polyline points="14 2 14 8 20 8"></polyline>
                     <line x1="12" y1="18" x2="12" y2="12"></line>
                     <line x1="9" y1="15" x2="15" y2="15"></line>
-                  </svg>
-                  <input
-                    type="radio"
-                    name="income-expense"
-                    value="income"
-                    checked={selectedOption === "income"}
-                    onChange={() => setSelectedOption("income")}
-                    style={{
-                      position: "absolute",
-                      opacity: 0,
-                      width: "100%",
-                      height: "100%",
-                      left: "87px",
-                      top: "34px",
-                      margin: 0,
-                      cursor: "pointer",
-                      zIndex: 50,
-                    }}
-                  />{" "}
+                  </svg>{" "}
                   Income
                 </label>
               </div>
@@ -239,25 +220,7 @@ const InputTranSactions: React.FC<InputTransactionsProps> = ({
                     <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
                     <polyline points="14 2 14 8 20 8"></polyline>
                     <line x1="9" y1="15" x2="15" y2="15"></line>
-                  </svg>
-                  <input
-                    type="radio"
-                    name="income-expense"
-                    value="expense"
-                    checked={selectedOption === "expense"}
-                    onChange={() => setSelectedOption("expense")}
-                    style={{
-                      position: "absolute",
-                      opacity: 0,
-                      width: "100%",
-                      height: "100%",
-                      right: "36px",
-                      top: "34px",
-                      margin: 0,
-                      cursor: "pointer",
-                      zIndex: 50,
-                    }}
-                  />{" "}
+                  </svg>{" "}
                   Expense
                 </label>
               </div>
